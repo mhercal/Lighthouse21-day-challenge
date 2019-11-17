51d17df2ead661f4b79bb17bc45cd0584f613bb1
mhercal <mhercal@users.noreply.github.com>
2019-11-17 11:28:42 +0100

    Create Day5.js

---
+++ Day5.js
@@ -0,0 +1,15 @@
+const chooseStations = (stations) => {
+  
+  let appropirateVotingStations = [];
+  
+  for (const station of stations) {
+    
+    const [name, capacity, venueType] = station;
+  
+    if (capacity >= 20 && (venueType === 'school' || venueType === 'community centre')) {
+      appropirateVotingStations.push(name);
+    }
+  }
+  
+  return appropirateVotingStations;
+}
